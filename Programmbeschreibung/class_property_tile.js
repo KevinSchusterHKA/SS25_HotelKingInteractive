013be7906b2df4288d0b7c5f8a54e58927f53aa5
F31330xdrive <gsdd9996@outlook.com>
2025-05-23 17:09:55 +0200

    Add files via upload

---
+++ Programmbeschreibung/class_property_tile.js
@@ -0,0 +1,15 @@
+var class_property_tile =
+[
+    [ "PropertyTile", "class_property_tile.html#acfebcb3b5aee00fd414aebd29458d483", null ],
+    [ "calculateRent", "class_property_tile.html#a64e34b95893712c80490415a526c0fde", null ],
+    [ "displayInfo", "class_property_tile.html#a61c27cd7dca89358cfccda10514477d0", null ],
+    [ "getBuildingLevel", "class_property_tile.html#a9c5b1f1562117a4d1a5b0668d5ea4cb9", null ],
+    [ "getGroupId", "class_property_tile.html#ad9d7f2de14cb8afd57966d87b7b76dd6", null ],
+    [ "getOwnerId", "class_property_tile.html#a42e06e59591c81d5ec15742b93d28596", null ],
+    [ "getPrice", "class_property_tile.html#a28eaebbd6e2945041eb6bde7c941b89b", null ],
+    [ "getPropertyType", "class_property_tile.html#a1486d5a4437fa8081e3f08db35d74950", null ],
+    [ "getRent", "class_property_tile.html#aeb822dafa93c0056a6c13cd9dbac20c1", null ],
+    [ "getTypeString", "class_property_tile.html#a72c8d022ea5aaeee7807e89074563dd3", null ],
+    [ "setOwner", "class_property_tile.html#a884168b57086d083aa9b0aaef3b3f039", null ],
+    [ "setRentLevels", "class_property_tile.html#a873c3d520bf0881f79c40cdeec377f9d", null ]
+];
---
+++ Programmbeschreibung/class_tile.js
@@ -0,0 +1,11 @@
+var class_tile =
+[
+    [ "Tile", "class_tile.html#a49cd5dc5215809255a4b6e05fa3e08b4", null ],
+    [ "~Tile", "class_tile.html#aecd90209e168f26bcdf36c2e445f424a", null ],
+    [ "displayInfo", "class_tile.html#a6b4a178138fdfc16cbf8e4e3bb56b71d", null ],
+    [ "getId", "class_tile.html#ac382874a902694c3ce05c84b5d0f6731", null ],
+    [ "getName", "class_tile.html#a303e020a835786b276540615a2e5c4eb", null ],
+    [ "getTypeString", "class_tile.html#a4b7dd6f84cedb637b59b05b9bf8a12c9", null ],
+    [ "id", "class_tile.html#a32d00999ec6ae098a2db62bf4c2c5e7c", null ],
+    [ "name", "class_tile.html#aa5408d0f0f4a60f25796f651db2f84ac", null ]
+];
