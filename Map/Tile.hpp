18494e30e44b073fc023c017f245fe6819394c43
Kevin <kevin.schuster@siemens.com>
2025-05-23 17:25:09 +0200

    first draft of map

---
+++ Map/Tile.hpp
@@ -0,0 +1,24 @@
+#ifndef TILE_HPP
+#define TILE_HPP
+
+#include <string>
+#include <memory>
+
+// 基类 Tile
+class Tile {
+protected:
+    int id;
+    std::string name;
+
+public:
+    Tile(int id, const std::string& name) : id(id), name(name) {}
+    virtual ~Tile() = default;
+
+    virtual void displayInfo() const = 0;
+
+    int getId() const { return id; }
+    std::string getName() const { return name; }
+    virtual std::string getTypeString() const; 
+};
+
+#endif
