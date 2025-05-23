b8f67483d048eb9b3ad0ac55f21d4aff462608c9
Kevin <kevin.schuster@siemens.com>
2025-05-23 17:39:36 +0200

    initial start of server

--- HKI.cpp
+++ HKI.cpp
@@ -2,19 +2,26 @@
 //
 
 #include <iostream>
+//#include "player.h"
+//#include "map.h"
+//#include "controls.h"
+//#include "config.hpp"
+
+using namespace std;
+
+#define GAMEVERSION "00.01"
+
 
 int main()
 {
-    std::cout << "Hotel King Interactive T0.1\n";
+    cout << "Hotel King Interactive T0.1\n";
+
+    //initial game
+    //start game
+    //check game results
+    //check winner
+
+    cout << "Thanks for playing Hotel King " << GAMEVERSION << endl;
+    //end game
 }
 
-// Programm ausführen: STRG+F5 oder "Debuggen" > Menü "Ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
