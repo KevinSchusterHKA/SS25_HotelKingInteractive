d437c34e1e785c4d4930e51c14308adf55d250a5
JeremyPochon <jeremp92@gmail.com>
2025-06-03 20:16:51 +0200

    Update test_main.cpp
    test writeLog

--- test_main.cpp
+++ test_main.cpp
@@ -1,16 +1,25 @@
 #include "Configuration.h"
 #include <iostream>
 using namespace std;
+#define ROUND		10
 
 int main() {
     Configuration config;
 
+/***************************************   test loadConfig  **********************************************/
     if (config.loadConfig("config.txt")) {
         cout << "Konfiguration erfolgreich geladen:\n";
         config.printSettings();
     }
     else {
         cout << "Fehler beim Laden der Konfiguration.\n";
+    }
+
+/***************************************   test writeLog  **********************************************/
+    for (int i = 0; i < ROUND; i++) {
+        config.writeLog(i+1, 1, 200 + i, "egal", i + 5);  //spieler1
+        config.writeLog(i+1, 2, 800 + i, "egal", i + 2);  //spieler2
+        config.writeLog(i+1, 3, 2000 + i, "egal", i + 4);  //spieler3
     }
 
     return 0;
