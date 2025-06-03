149141275eda85105227d035ca87dad59e448912
F31330xdrive <gsdd9996@outlook.com>
2025-06-03 20:21:37 +0800

    Delete Map directory

--- Map/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Map.hpp"
-
-int main() {
-    GameMap gameMap;
-    gameMap.displayMap();
-
-    int next = gameMap.getNextTile(3, 7);
-    std::cout << "Spieler landet auf Feld: " << gameMap.getTile(next).name << "\n";
-}
