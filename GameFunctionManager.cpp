e0a72ab914a95c4178a32baf5e65806f4c21faec
Sami <53356202+imas999@users.noreply.github.com>
2025-06-10 15:11:22 +0200

    Updated Version of Controls. Implementierung der Mapgebundenen Funktionen

---
+++ GameFunctionManager.cpp
@@ -0,0 +1,241 @@
+/*
+File : GameFunctionManager.cpp 
+Description : Manager for game functions including dice rolling, buying and managing players.
+
+Author : Sami El Aidi 
+Date : 2025-05-26
+
+Version : 1.1
+
+Changes: 
+ - Slight changes to the rollDice()-Method:
+    - Now Prints the Result in the Terminal
+
+Notes: 
+ -  The class handles game-related functions such as rolling dice, checking for a pasch, and managing players.
+ -  Doesnt need to be called directly, but is used by the MenuManager to handle game logic.
+*/
+
+
+#include <conio.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <random>
+#include <ctime>
+
+#include "GameFunctionManager.hpp"
+#include "Player.hpp"
+#include "Map.hpp"
+
+using namespace std;;
+
+/*
+Clears the console screen.
+*/ 
+void GameFunctionManager::clear_screen() { cout << "\x1B[2J\x1B[H";}
+
+/*
+Private function to generate a random number between 1 and 6.
+@return A random number between 1 and 6.
+*/
+int GameFunctionManager::randomNumber() {
+    static mt19937 generator(static_cast<unsigned int>(time(nullptr))); 
+    static uniform_int_distribution<int> distribution(1, 6); 
+    return distribution(generator);
+}
+
+/*
+Generator funktion to create a new GameFunctionManager object.
+@param current_player The index of the current player. (Normal is 0 at start)
+*/
+GameFunctionManager::GameFunctionManager() {
+    this->current_player = current_player;
+    this->pasch_counter = 0; 
+    this->current_player = 0; 
+    vector<Player> players; 
+    GameMap map; 
+}
+
+/*
+Function to add a player to the game.
+Note: Needs a check if the player already exists in the vector.
+@param player The Player object to be added.
+*/
+void GameFunctionManager::addPlayer(Player player) {
+    players.push_back(player);
+}
+
+
+/**
+*Displays all of the information of the current player.
+*@param player The Player object whose information is to be displayed.
+*/
+void GameFunctionManager::showPlayerInformation(Player player) {
+    clear_screen();
+    cout << "Player " << (getCurrentPlayer() + 1) << ": " << getPlayers()[getCurrentPlayer()].getName() << endl; 
+    cout << "Position: " << getPlayers()[getCurrentPlayer()].getPosition() << endl;
+    cout << "Budget: " << getPlayers()[getCurrentPlayer()].getMoney() << endl;
+    cout << "----------------------------" << endl;
+    cout << "Ereigniskarten: ";
+    int counter = 1;
+    for (auto item : player.getKarten()) {
+        cout << counter << ") " << item << endl;
+        counter++;
+    }
+    cout << " " << endl;
+    cout << "----------------------------" << endl;
+    cout << "Grundstücke: " << endl;
+    for (int i = 0; i < 40; i++) {
+        PropertyTile* propertyTile = dynamic_cast<PropertyTile*>(getMap().getTile(i).get());
+        if (propertyTile && propertyTile->getOwnerId() == player.getID()) {
+            cout << propertyTile->getName() << " - Miete: " <<propertyTile->getRent() << endl;
+        }
+    }
+}
+
+/**
+ * Displays information about the tile at the given index.
+ * @param tile The index of the tile to display information for.
+ * This function clears the screen and prints the name, owner, price, and rent levels of the tile.
+ */
+void GameFunctionManager::showTileInfomation(int tile) {
+    clear_screen();
+    auto& sTile = getMap().tiles[tile];
+
+     if (auto propTile = dynamic_cast<const PropertyTile*>(sTile.get())) {
+        cout << "---------" << propTile->getName() << "---------" << endl;
+        cout << "Besitzer: " << ((propTile->getOwnerId() == -1) ? "Kein Besitzer" : getPlayers()[propTile->getOwnerId() + 1].getName()) << endl;
+        cout << " Preis : " << propTile->getPrice() << endl;
+        cout << " Miete: " << endl;
+        int counter = 1;
+        for (int rent : propTile->getRentLevels()) {
+            cout << "       Stufe " << counter << ": " << rent << endl;
+            counter++;
+        }
+    } else if (auto specialTile = dynamic_cast<const SpecialTile*>(sTile.get())) {
+        std::cout << "  Name: " << specialTile->getName() << "\n"
+                  << "  Category: " << specialTile->getTypeString() << "\n";
+    }
+}
+
+/*
+Prints the current map of the game in the console.
+*/
+void GameFunctionManager::showMap() {
+    clear_screen();
+    cout << "+--------------+-----------+----------+----------+----------+----------+------------+--------------+-----------+--------------+---------------+" << endl;
+    cout << "| Gefängnis    | Amaliens. |  E-Werk  | Hirschs. | Kriegss. | West-Bhf | Fastplatz  | Gemeinschaft | Kaiser A. | Durlacher A. | Frei Parken   |" << endl;  
+    cout << "+--------------+-----------+----------+----------+----------+----------+------------+--------------+-----------+--------------+---------------+" << endl;
+    cout << "| Ettlingers.  |                                                                                                              |   Zirkel      |" << endl;
+    cout << "+--------------+                                                                                                              +---------------+" << endl;
+    cout << "| Rüppurers.   |                                                                                                              | Gemeinschaft  |" << endl; 
+    cout << "+--------------+                                                                                                              +---------------+" << endl;
+    cout << "| Ereignis     |                                                                                                              |    Karlss.    |" << endl;
+    cout << "+--------------+                                                                                                              +---------------+" << endl;
+    cout << "| Eberts.      |                                                                                                              |   Brauers.    |" << endl;
+    cout << "+--------------+                                                                                                              +---------------+" << endl;
+    cout << "| Haupt-Bhf    |                                                                                                              |   Ost-Bhf     |" << endl;
+    cout << "+--------------+                                                                                                              +---------------+" << endl;
+    cout << "|   Steuer     |                                                                                                              | Hildaprom.    |" << endl;
+    cout << "+--------------+                                                                                                              +---------------+" << endl;
+    cout << "|   Adlers.    |                                                                                                              |   Moltkes.    |" << endl; 
+    cout << "+--------------+                                                                                                              +---------------+" << endl;
+    cout << "| Gemeinschaft |                                                                                                              |    W-Werk     |" << endl;
+    cout << "+--------------+                                                                                                              +---------------+" << endl;
+    cout << "|   Kronens.   |                                                                                                              | Karl-Friedr.  |" << endl;
+    cout << "+--------------+-----------+----------+----------+----------+----------+------------+--------------+-----------+--------------+---------------|" << endl;
+    cout << "|    LOS!      | Schlossp. |  Steuer  | Kaisers. | Ereignis | Helipad  | Erbprinzs. | Gemeinschaft |   Walds.  |   Herrens.   |  Ins Gefängis |" << endl;
+    cout << "+--------------+-----------+----------+----------+----------+----------+------------+--------------+-----------+--------------+---------------+" << endl;
+
+
+}
+
+/*
+Function to roll the dice and return the result.
+The function simulates a dice roll by generating random numbers between 1 and 6.
+@return A vector containing the results of the two dice rolls.
+*/
+vector<int> GameFunctionManager::rollDice() {
+    vector<int> dice(2);
+    for (int i = 0; i < (randomNumber() * 3); i++) {
+        clear_screen();
+        int random1 = randomNumber();
+        int random2 = randomNumber();
+
+        cout << " ---       --- " << endl;
+        cout << "| " << random1 << " |  " << "   | " << random2 << " |" << endl;
+        cout << " ---       --- " << endl;
+        
+        if (i = (rand() & 6 + 10) - 1) {
+            dice[0] = random1;
+            dice[1] = random2;
+        }
+    
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+    getPlayers()[getCurrentPlayer()].move(dice[0] + dice[1]);
+
+    showTileInfomation(getPlayers()[getCurrentPlayer()].getPosition());
+    cout << "----------------------------" << endl;
+    cout << "Du hast eine " << dice[0] << " und eine " << dice[1] << " gewürfelt!" << endl;
+    if (checkPasch(dice)) {
+        cout << "Du hast einen Pasch gewürfelt! Päsche geworfen: " << pasch_counter << endl;
+    } 
+    this_thread::sleep_for(chrono::milliseconds(5000));
+    
+    return dice;
+}
+
+/*
+Checks if the rolled dice are a pasch (both dice show the same number).
+@param dice A vector containing the results of the two dice rolls.
+@return True if the dice are a pasch, false otherwise.
+*/
+bool GameFunctionManager::checkPasch(vector<int> dice) {
+    return (dice[0] == dice[1]);
+}
+
+/*
+Getter function for the current player.
+@return The index of the current player.
+*/
+int GameFunctionManager::getCurrentPlayer() {return current_player;}
+
+/*
+Getter function for the players vector.abort
+@return A pointer to the vector of Player objects.
+Note: This function returns a pointer to the vector, allowing direct access to the vector.
+*/
+vector<Player>& GameFunctionManager::getPlayers() {return players;}
+
+/*
+Getter function for the pasch counter.
+@return The current value of the pasch counter.
+*/
+int GameFunctionManager::getPaschCounter() {return pasch_counter;}
+
+/*
+ Setter function for the current player.
+ @param player The index of the new current player. 
+ */
+void GameFunctionManager::setCurrentPlayer(int player) {current_player = player;}
+
+/*
+ Setter function for the pasch counter.
+ @param counter The new value for the pasch counter. 
+ */
+void GameFunctionManager::setPaschCounter(int counter) {pasch_counter = counter;}
+
+int  GameFunctionManager::getCurrentRound() {return current_round;}
+
+void GameFunctionManager::setCurrentRound(int round) {current_round = round;}
+
+GameMap& GameFunctionManager::getMap() {return map;}
+
+
---
+++ MenuManager.cpp
@@ -0,0 +1,375 @@
+/*
+File : MenuManager.cpp
+Description : Implementation file for MenuManager class
+
+Author : Sami El Aidi 
+Date : 2025-05-26
+
+Version : 1.1
+
+Changes:
+ - Added doOperation()-Method in which the functionality of the menu will be implemented. 
+ - Currently Implemented:
+    - Roll Dice in the InGame-Menu 
+    - Show Highscores in the Main menu
+
+Notes: 
+ -  This file contains the implementation of the MenuManager class, which manages the menu system for the Hotel King Interactive game.
+ -  The class includes methods for initializing menus, handling menu navigation, and managing game functions.
+ -  To use this class, create an instance of MenuManager and call the handleMenus() method to start the menu system.
+*/
+
+#include <conio.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <random>
+#include <ctime>
+
+#include "MenuManager.hpp"
+#include "Player.hpp"
+#include "Map.hpp"
+
+using namespace std;
+
+/*
+Constructor for the MenuManager class.
+Initializes the menu system and opens the log file for menu operations.
+*/
+MenuManager::MenuManager() : current_Layer(0), current_menu(nullptr), MenuLog() {
+    this->MenuLog.open("MenuLog.txt", ios::trunc);
+    initMenus();
+    this->inGame = false;
+    this->gameFunctionManager = new GameFunctionManager();
+}
+
+/*
+Clears the console screen.
+*/ 
+void MenuManager::clear_screen() { cout << "\x1B[2J\x1B[H";}
+
+/*
+Initializes the menu system with a start menu and submenus.
+*/
+void MenuManager::initMenus() {
+
+    //Main Menu
+    Menu startMenu(0, "Hotel King Interactive", {"Neues Spiel", "Spiel laden", "Highscores", "Exit"}, false);
+    //Menu newGameMenu(1, "Neues Spiel starten", {"Anzahl Spieler", "Regeln einstellen", "Spiel starten", "Zurück"}, true);
+    //Menu loadGameMenu(1, "Spiel laden",  {"Spiel laden", "Zurück"}, true);
+    
+    //Ingame Overlay Menu
+    Menu inGameMenu(0, "Ingame", {"Würfeln", "Handeln und Tauschen", "Inventar anzeigen", "Feld anzeigen", "Spielbrett anzeigen", "Spielstand speichern", "Exit"}, true);
+
+    menus.push_back(startMenu);
+    menus.push_back(inGameMenu);
+
+    if (!menus.empty()) current_menu = &menus[0]; 
+
+    //getCurrentMenu().addSubmenu(newGameMenu);
+    //getCurrentMenu().addSubmenu(loadGameMenu);
+
+    getMenulog() << "Menu: " << menus[0].getHeader() << " initialized with " << menus[0].getMenuItems().size() << " items." << endl;
+    getMenulog() << "Menu: " << menus[1].getHeader() << " initialized with " << menus[1].getMenuItems().size() << " items." << endl;
+}
+
+/**
+ * Check if the current menu has a submenu and if the current position is not the first one
+ * @return true if the last menu item is "Exit" (quit application on Layer 0) if in submenu, the return to the main menu.
+ */
+bool MenuManager::escapeCheck() {;
+    if (getCurrentLayer() == 0 && getCurrentMenu().getMenuItems()[getCurrentMenu().getCurrentPosition()] == "Exit") {
+        getMenulog() << "Enter: END" << endl;
+        exit(0); // Exit the application
+    }  
+
+    if (getCurrentMenu().getCurrentPosition() == getCurrentMenu().getMenuItems().size() - 1) {           
+        getMenulog() << "Esc: " << " Current Position: " << getCurrentMenu().getCurrentPosition() << ", Menu Items Size: " << (getCurrentMenu().getMenuItems().size() - 1) << ", Submenu Status: " << getCurrentMenu().hasSubmenu() << endl;
+        setCurrentLayer(0);
+        setCurrentMenu(menus[0]); 
+        return true;
+    }
+   return false;         
+}
+
+/*
+Adds a menu to the MenuManager's list of menus.
+@param menu The Menu object to be added.
+*/
+void MenuManager::addMenu(Menu& menu) {menus.push_back(menu);}
+
+void MenuManager::doOperation(char input) {
+    if (input == 13 && isInGame()) {
+        switch (getCurrentMenu().getCurrentPosition()) {
+            case 0: {
+                //Würfeln
+                vector<int> roll = getGameFunctionManager().rollDice();
+                getGameFunctionManager().checkPasch(roll) ? gameFunctionManager->setPaschCounter(getGameFunctionManager().getPaschCounter() +1) : getGameFunctionManager().setPaschCounter(0);
+                break;
+            }
+            case 1: {
+                //Handeln und Tauschen
+                break;
+            }
+            case 2: {
+                //Inventar anzeigen
+                getGameFunctionManager().showPlayerInformation(getGameFunctionManager().getPlayers()[getGameFunctionManager().getCurrentPlayer()]);
+                this_thread::sleep_for(chrono::milliseconds(10000));
+                break;
+            }
+            case 3: {
+                //Momentanes Feld anzeigen
+                getGameFunctionManager().showTileInfomation(getGameFunctionManager().getPlayers()[getGameFunctionManager().getCurrentPlayer()].getPosition());
+                this_thread::sleep_for(chrono::milliseconds(10000));
+                break;
+            }
+            case 4: {
+                //Spielbrett anzeigen
+                getGameFunctionManager().showMap();
+                this_thread::sleep_for(chrono::milliseconds(10000));
+                break;
+            }
+            case 5: {
+                //Spielstand speichern
+                break;
+            }
+            default: {
+                break;                        
+            }
+        }     
+    } else if (input == 13 && !isInGame() && getCurrentLayer() == 0 && !getCurrentMenu().hasSubmenu()) {
+        switch (getCurrentMenu().getCurrentPosition()) {
+            case 0: {
+                //Spiel starten
+                //Server.stardialog();
+                cout << "Spielername: " << endl;
+                string name;
+                cin >> name;
+                Player player = Player(name, 1500, 0);
+                getGameFunctionManager().addPlayer(player);
+                getGameFunctionManager().setCurrentPlayer(0);
+                getGameFunctionManager().setCurrentRound(1);
+                setInGame(true);
+                setCurrentMenu(getMenus()[1]); //Switch to Ingame Menu
+                break;
+            }    
+            case 1: {
+                //Spiel laden
+                break;
+            }    
+            case 2: {
+                //Highscores anzeigen
+
+                //Will be implemented when Config implementation is uploaded to Github
+                //Asuming the Higscorelist in the txt-Document is allready sorted
+                
+                /*
+                vector<Score> highscores = Configuration.getHighscores(); 
+                int counter = 1;
+                for (auto score : highscores) {
+                    cout << counter << ") " << score.getPlayerName() << " - " << score.getFinalBudget() << endl;
+                    counter++;
+                }
+                */
+
+                //For test purposes (will be deleated later):
+                clear_screen();
+                ifstream highscores("HighscoresTest.txt");
+                if (highscores.is_open()) {
+                    string line;
+                    while (getline(highscores, line)) {
+                        cout << line << endl;
+                    }
+                }    
+                this_thread::sleep_for(chrono::milliseconds(10000));
+                highscores.close(); 
+                break;
+            }    
+            default:
+                break;    
+        }
+    }
+}
+
+/**
+ * Controls the input and navigation through the before game (Startmenu, Settings, etc.) menu system. 
+ * Simple use of arrow keys for navigation and enter key to select options. 
+ * For use, only initialize the MenuManager and call the handleMainMenu() method.
+ */
+void MenuManager::handleMenus() {
+        while (true) {
+        getMenulog() << "Ingame: " << (isInGame() ? "Yes" : "No") << " - Current Menu: " << getCurrentMenu().getHeader() << endl;
+        if (isInGame()) {
+            getCurrentMenu().displayIngameMenu(*gameFunctionManager);
+        } else {
+            getCurrentMenu().displayMainMenu();
+        }
+        
+        char input = _getch(); // Get user input
+        if (input == 13) { 
+            // Enter key
+            switch (getCurrentMenu().getCurrentPosition()) {
+                /**
+                 * Opens the submenu based on the current position, if there is a submenu available.
+                 * The order of the cases corresponds to the position in the menu and the position of the submenu in the Submenu vector.
+                 * Currently, it only supports up to 3 submenus. Extencion via adding more cases.
+                 */
+                case 0: 
+                    if (escapeCheck()) break;
+                    if (getCurrentMenu().hasSubmenu() && getCurrentMenu().getSubmenus().size() > 0) {
+                        setCurrentMenu(getCurrentMenu().getSubmenus()[0]); 
+                        getCurrentMenu().setCurrentPosition(0); 
+                        setCurrentLayer(getCurrentMenu().getLayer() + 1); 
+
+                        getMenulog() << "Enter: " << "Current Menu: " << getCurrentMenu().getHeader() << ", Current Position: " << getCurrentMenu().getCurrentPosition() << ", Current Layer: " << getCurrentLayer() << endl;
+                    }
+                    break;
+                case 1:
+                    if (escapeCheck()) break;
+                    if (getCurrentMenu().hasSubmenu() && getCurrentMenu().getSubmenus().size() > 1) {
+                        setCurrentMenu(getCurrentMenu().getSubmenus()[1]); 
+                        getCurrentMenu().setCurrentPosition(0); 
+                        setCurrentLayer(getCurrentMenu().getLayer() + 1); 
+
+                        getMenulog() << "Enter: " << "Current Menu: " << getCurrentMenu().getHeader() << ", Current Position: " << getCurrentMenu().getCurrentPosition() << ", Current Layer: " << getCurrentLayer() << endl;
+                    }
+                    break;
+                case 2: 
+                    if (escapeCheck()) break;
+                    if (getCurrentMenu().hasSubmenu() && getCurrentMenu().getSubmenus().size() > 2) {
+
+                        getMenulog() << "Enter: " << "Current Menu: " << getCurrentMenu().getHeader() << ", Current Position: " << getCurrentMenu().getCurrentPosition() << ", Current Layer: " << getCurrentLayer()<< endl;
+                    }
+                    break;
+                case 3:  
+                    if (escapeCheck()) break;
+                    if (getCurrentMenu().hasSubmenu()  && getCurrentMenu().getSubmenus().size() > 3) {
+                        setCurrentMenu(getCurrentMenu().getSubmenus()[3]); 
+                        getCurrentMenu().setCurrentPosition(0); 
+                        setCurrentLayer(getCurrentMenu().getLayer() + 1); 
+
+                        getMenulog() << "Enter: " << "Current Menu: " << getCurrentMenu().getHeader() << ", Current Position: " << getCurrentMenu().getCurrentPosition() << ", Current Layer: " << getCurrentLayer() << endl;
+                    } 
+                    break; 
+                case 4:  
+                    if (escapeCheck()) break;
+                    if (getCurrentMenu().hasSubmenu()  && getCurrentMenu().getSubmenus().size() > 4) {
+                        setCurrentMenu(getCurrentMenu().getSubmenus()[4]); 
+                        getCurrentMenu().setCurrentPosition(0); 
+                        setCurrentLayer(getCurrentMenu().getLayer() + 1); 
+
+                        getMenulog() << "Enter: " << "Current Menu: " << getCurrentMenu().getHeader() << ", Current Position: " << getCurrentMenu().getCurrentPosition() << ", Current Layer: " << getCurrentLayer() << endl;
+                    } 
+                    break; 
+                case 5:  
+                    if (escapeCheck()) break;
+                    if (getCurrentMenu().hasSubmenu()  && getCurrentMenu().getSubmenus().size() > 5) {
+                        setCurrentMenu(getCurrentMenu().getSubmenus()[5]); 
+                        getCurrentMenu().setCurrentPosition(0); 
+                        setCurrentLayer(getCurrentMenu().getLayer() + 1); 
+
+                        getMenulog() << "Enter: " << "Current Menu: " << getCurrentMenu().getHeader() << ", Current Position: " << getCurrentMenu().getCurrentPosition() << ", Current Layer: " << getCurrentLayer() << endl;
+                    } 
+                    break; 
+                case 6:  
+                    if (escapeCheck()) break;
+                    if (getCurrentMenu().hasSubmenu()  && getCurrentMenu().getSubmenus().size() > 6) {
+                        setCurrentMenu(getCurrentMenu().getSubmenus()[6]); 
+                        getCurrentMenu().setCurrentPosition(0); 
+                        setCurrentLayer(getCurrentMenu().getLayer() + 1); 
+
+                        getMenulog() << "Enter: " << "Current Menu: " << getCurrentMenu().getHeader() << ", Current Position: " << getCurrentMenu().getCurrentPosition() << ", Current Layer: " << getCurrentLayer() << endl;
+                    } 
+                    break;         
+                default:
+                    if (escapeCheck()) break;
+                    getMenulog() << "Invalid: " << input << ", Current Menu: " << getCurrentMenu().getHeader() << endl;
+                    break;
+            }
+           
+        } else if (input == 72 && getCurrentMenu().getCurrentPosition() > 0) {
+            // Up arrow key
+            getMenulog() << "Up: " << input << ", Current Menu: " << getCurrentMenu().getHeader() << ", Current Position: " << getCurrentMenu().getCurrentPosition() << endl;
+
+            getCurrentMenu().setCurrentPosition(getCurrentMenu().getCurrentPosition() - 1);
+
+            if (isInGame()) {
+                getCurrentMenu().displayIngameMenu(*gameFunctionManager);
+            } else {
+                getCurrentMenu().displayMainMenu();
+            }
+            
+        } else if (input == 80 && getCurrentMenu().getCurrentPosition() < (getCurrentMenu().getMenuItems().size() - 1)) { 
+            // Down arrow key
+            getMenulog() << "Down: " << input << ", Current Menu: " << getCurrentMenu().getHeader() << ", Current Position: " << getCurrentMenu().getCurrentPosition() << endl;
+
+            getCurrentMenu().setCurrentPosition(getCurrentMenu().getCurrentPosition() + 1);
+            
+            if (isInGame()) {
+                getCurrentMenu().displayIngameMenu(*gameFunctionManager);
+            } else {
+                getCurrentMenu().displayMainMenu();
+            }
+        }
+        doOperation(input);
+    }	
+} 
+
+/*
+Gets the GameFunctionManager instance managed by the MenuManager.
+@return A reference to the GameFunctionManager instance.
+*/
+GameFunctionManager& MenuManager::getGameFunctionManager() {return *gameFunctionManager;}
+
+/*
+Gets the current layer of the menu.
+@return The current layer of the menu.
+*/
+Menu& MenuManager::getCurrentMenu() {return *current_menu;}
+
+/*
+Sets the current layer of the menu.
+@param layer The new layer to be set.
+*/
+void MenuManager::setCurrentMenu(Menu& menu) {current_menu = &menu;}
+
+/*
+Returns the log for the menu Operations.
+@return The ofstream object for the menu log.
+*/
+ofstream& MenuManager::getMenulog() { return MenuLog; }
+
+/*
+Sets the current layer of the menu.
+@param layer The new layer to be set.
+*/
+void MenuManager::setCurrentLayer(int layer) {current_Layer = layer;}
+
+/*
+Returns the current layer of the menu.
+@return The current layer of the menu.
+*/
+int MenuManager::getCurrentLayer() {return current_Layer;}
+
+/*
+@return true if the game is currently in progress, false otherwise.
+*/
+bool MenuManager::isInGame() {return inGame;}
+
+/*
+Sets the in-game status of the menu manager.
+@param inGame The new in-game status to be set.
+*/
+void MenuManager::setInGame(bool inGame) {this->inGame = inGame;}
+
+/*
+Gets the vector of menus managed by the MenuManager.
+@return A reference to the vector of Menu objects.
+*/
+vector<Menu>& MenuManager::getMenus() {return menus;}
+
